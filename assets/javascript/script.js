a6500dceb152ed05f38d48054f660df22d27dfa4
cperez2187 <cperez2187@gmail.com>
2017-06-03 18:52:32 -0700

    Added places search functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	var map;
+	let map;
 	function initMap() {
 	    map = new google.maps.Map(document.getElementById('map'), {
 	      center: {lat: -34.397, lng: 150.644},
@@ -10,25 +10,50 @@ $(document).ready(function() {
 
 	initMap(); 
 
-	// Get Geolocating
+	// Get Geolocation
 	let geocoder = new google.maps.Geocoder();
 	let homeLatLng;
+	let service;
 
-	// Get geolocation for address
-	function codeAddress() {
-		let address = '1600 Amphitheatre Parkway, Mountain View, CA';
+	getDetails();
 
-		geocoder.geocode({address: address}, (results, status) => {
-			if (status == 'OK') {
-				homeLatLng = results[0].geometry.location;
-				console.log(homeLatLng.lat(), homeLatLng.lng());
-			} else {
-				alert('Geocode was not successful for the following reason: ' + status);
+	// 
+	function getDetails() {
+		let address = '555 S Galleria Way, Chandler, AZ';
+
+		// Get geolocation for address
+		geocoder.geocode({address: address}, geoCallBack);
+	}
+	
+	function geoCallBack(results, status) {
+		if (status == 'OK') {
+			homeLatLng = results[0].geometry.location;
+			console.log('homeLatLng: ' + homeLatLng);
+
+			// request object used to search for gas stations withi a 500 meter radius
+			let request = {
+				location: homeLatLng,
+				radius: '2500',
+				type: 'gas_station'
 			}
-		});
+			console.log(request);
+
+			// Do a Places nearby search using request info
+			service = new google.maps.places.PlacesService(document.createElement('div'));
+			service.nearbySearch(request, searchCallBack);
+
+		} else {
+			alert('Geocode was not successful for the following reason: ' + status);
+		}
 	}
 
-	codeAddress();
+	function searchCallBack(results, status) {
+		if (status == google.maps.places.PlacesServiceStatus.OK) {
+			console.log(results);
+		} else {
+			alert('Something went wrong: ' + status);
+		}
+	}
 });
 
 
